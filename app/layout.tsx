f7fa624c4e722fd10c41a8ef76b5a01b17e6288f
muzzammil-123 <146634572+muzzammil-123@users.noreply.github.com>
2025-08-15 17:10:40 +0500

    n

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,7 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Adrian's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  title: "Muzzammil's Portfolio",
 };
 
 export default function RootLayout({
